f90d9cbab3cdfbbd8dbb4bf2d04503963ef4e850
Zerra86 <111981309+Zerra86@users.noreply.github.com>
2022-09-08 10:39:57 -0300

    Add files via upload

---
+++ JavaScript/Lista_de_exercícios_JavaScript/PrimeiraQuestão/script.js
@@ -0,0 +1,11 @@
+const lampada = document.getElementById("lamp");
+
+function minhaFuncaoAcende() {
+    lampada.src = "lampOn.jpg"
+
+}
+   
+function minhaFuncaoDesliga() {
+    lampada.src = "lampOff.jpg"
+}
+
---
+++ JavaScript/arquivo.js
@@ -0,0 +1,42 @@
+/*
+JAVASCRIPT - CLIENT - SIDE
+HTML É ONDE eu coloco meu código em javascript.
+JS é popular em mais de 90% das páginas da internet
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
